ef5afd759fff62ab07f08bf8ff4c6a0830d6da2c
Cam <cameronmanzo111@gmail.com>
2024-08-01 20:04:48 -0400

    more work on components

--- sketch.ino
+++ sketch.ino
@@ -1,14 +1,19 @@
 #include <LiquidCrystal.h>
-#include <Keypad.h>
+#define LDR_PIN 2
+#define MOTION_PIN 16
+#define TEMP_PIN 26 // temperature 
 
-const uint8_t ROWS = 4;
-const uint8_t COLS = 3;
-char keys[ROWS][COLS] = {
-  { '1', '2', '3', 'A'},
-  { '4', '5', '6', 'B'},
-  { '7', '8', '9', 'C'},
-  { '*', '0', '#', 'D'}
-};
+int motionState = LOW;            
+int detectedMotion = 0;     
+const int motionCountTillWarning = 3;
+bool motionWarning = false;
+
+
+int lightState = HIGH;
+int isLit = 0;
+bool lightWarning = false;
+const int lightCountTilWarning = 5;
+
 
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 
@@ -23,33 +28,61 @@ uint8_t heart[8] = {
   0b00000,
 };
 
-uint8_t colPins[COLS] = { 21, 20, 19, 17 };
-uint8_t rowPins[ROWS] = { 28, 27, 26, 22 };
-
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-
-void setup() {
-  Serial.begin(9600);  // Start serial communication
-  lcd.init();
-  lcd.backlight();
+void setup() { 
+  pinMode(MOTION_PIN, INPUT);
+  pinMode(LDR_PIN, INPUT);
+  Serial1.begin(11520);  // Start serial communication
   lcd.begin(16, 2);
   
   lcd.createChar(0, heart);
   
 
   lcd.print("On");
-  Serial.println("Setup complete");
+
+
+  Serial1.println("Setup complete");
+
+if (isLit != 0) {
+  isLit = 0;
+}
+if (detectedMotion != 0) {
+  detectedMotion = 0;
+}
+  
 }
 
-void loop() {
-  char key = keypad.getKey();
-  
-  if (key != NO_KEY) {
-    Serial.print("Key pressed: ");
-    Serial.println(key);
-    lcd.clear();
-    lcd.print(key);
+void loop() { // enclose all in if so its only if button is pressed
+  // motion
+motionState = digitalRead(MOTION_PIN);
+  lcd.setCursor(2, 0);
+if (motionWarning == false) {
+  if (motionState == HIGH) {
+    lcd.print("MOTION DETECTED");
+    detectedMotion++;
   }
-  delay(10);
+  if (detectedMotion >= motionCountTillWarning) {
+    motionWarning = true;
+    lcd.print("OVER MOTION LIMIT");
+  }
+}
+
+
+  //light
+lightState = digitalRead(LDR_PIN);
+if (lightWarning == false) {
+  if (lightState == LOW) {
+    isLit++;
+    lcd.print(isLit);
+  }
+  if (isLit >= lightCountTilWarning) {
+      lightWarning = true;
+      
+  }
+}
+
+  
+
+
+  delay(200);
 
 }
