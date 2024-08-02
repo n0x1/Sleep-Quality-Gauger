956dfe6605b6136f1c35645529aa84e3b4f7d89f
Cam <cameronmanzo111@gmail.com>
2024-08-02 18:21:27 -0400

    rtc added tm does not work rtc not connecting idek anymore

--- sketch.ino
+++ sketch.ino
@@ -2,6 +2,7 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <RTClib.h>
 
 
 #define LDR_PIN 2
@@ -10,11 +11,21 @@
 #define SLIDER_PIN 27
 #define DHT_PIN 4 // sda pin
 #define BUZZER_PIN 17
+#define DIO 22
+#define CLK 21
 
 DHT dht(DHT_PIN, DHT22);
+RTC_DS1307 rtc;
 
-const int ALARM_HOURS = 8; // IN HOURS, after going to sleep. 8 recomended
-bool alarmNotStopped = true;
+
+int runCount = 0;
+
+const int ALARM_HOURS = 8; // CHANGE THIS IN HOURS// Time to set off alarm after turning on. 8 is recomended
+
+
+bool alarmTriggered = false;
+unsigned long alarmStartTime = 0; 
+
 int cursorX = 0;
 
 //motion
@@ -37,7 +48,7 @@ int tempTooLowCount = 0;
 const int tempHighCountTilWarning = 6;
 const int tempLowCountTilWarning = 6;
 const float TEMP_THRESHOLD_HIGH = 23.9; // (celsius) range can be changed but this is good for safety from research  
-const float TEMP_THRESHOLD_LOW = 12.7;
+const float TEMP_THRESHOLD_LOW = 12.5;
 
 //humidity
 bool humidHighWarning = false;
@@ -50,8 +61,7 @@ const int HUMID_THRESHOLD_HIGH = 50; // these ones are percents
 const int HUMID_THRESHOLD_LOW = 30; // 30-50% is recommended 
 
 
-bool running = true; // disable to stop the loop
-
+bool running = true; // disable to stop the loopconst int startTime = millis()
 
 
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
@@ -77,37 +87,53 @@ uint8_t smiley[8] = {
   0b00000
 };
 
+
+
 void setup() {
+  
   pinMode(MOTION_PIN, INPUT);
   pinMode(LDR_PIN, INPUT);
   pinMode(SLIDER_PIN, INPUT);
   pinMode(TEMP_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
 
+    lcd.createChar(0, heart);
+    lcd.createChar(1, smiley);
     lcd.begin(16, 2);
-      lcd.createChar(0, heart);
-      lcd.createChar(1, smiley);
-    lcd.print("Slide UP to enable"); 
-    lcd.write(byte(0)); // heart
-    lcd.write(byte(1)); // :)
-    delay(1000);
-    lcd.clear();
 
+    lcd.print("Good night "); 
+    lcd.write(byte(0)); // heart
+    lcd.setCursor(0,1);
+    lcd.print("Slide UP ");
+    lcd.write(byte(1)); // :)
+    delay(4500);
+    lcd.setCursor(0,0);
+    lcd.clear();
 
    dht.begin(); 
 
-  // setAlarm();
+   alarmStartTime = millis() + (ALARM_HOURS * 60 * 60 * 1000); // ms
+
+       if (!rtc.begin()) {
+        lcd.print("Couldn't find RTC");
+    }
+
 }
 
-void loop() { // enclose all in if so its only if switch is high
-
+void loop() {
   int sliderValue = analogRead(SLIDER_PIN);
-  unsigned long time = millis();
 
   motionState = digitalRead(MOTION_PIN);
    lightState = digitalRead(LDR_PIN);
 
+
+  if (runCount == 0) {
+    lcd.print("No issues"); 
+    lcd.write(byte(0)); // heart
+    lcd.write(byte(1)); // :)
+  }
   if (sliderValue > 700 && running == true) {
+
 
     //motion
     if (motionWarning == false) {
@@ -132,7 +158,7 @@ void loop() { // enclose all in if so its only if switch is high
       if (!lightWarning && isLit >= lightCountTilWarning) {
         lightWarning = true;
 
-        lcd.print("Bright");
+        printWithFormatting("Bright");
       }
     }
 
@@ -146,7 +172,7 @@ void loop() { // enclose all in if so its only if switch is high
       }
       if (!tempHighWarning && tempTooHighCount >= tempHighCountTilWarning) {
           tempHighWarning = true;
-          lcd.print("Hot");
+          printWithFormatting("Hot");
       }
     }
         if (tempLowWarning == false) {
@@ -159,7 +185,7 @@ void loop() { // enclose all in if so its only if switch is high
       }
       if (!tempLowWarning && tempTooLowCount >= tempLowCountTilWarning) {
           tempLowWarning = true;
-          lcd.print("Cold");
+          printWithFormatting("Cold");
       }
 
     }
@@ -171,10 +197,11 @@ void loop() { // enclose all in if so its only if switch is high
       if (humidity > HUMID_THRESHOLD_HIGH) {
           humidTooHighCount++;
           delay(1000);
+          tone(BUZZER_PIN, 244, 15); // debug
       }
       if (!humidHighWarning && humidTooHighCount >= humidHighCountTilWarning) {
           humidHighWarning = true;
-          lcd.print("Humid");
+          printWithFormatting("Humid");
       }
     }
         if (humidLowWarning == false) {
@@ -186,7 +213,7 @@ void loop() { // enclose all in if so its only if switch is high
       }
       if (humidLowWarning == false && humidTooLowCount >= humidLowCountTilWarning) {
           humidLowWarning = true;
-          lcd.print("Dry");
+          printWithFormatting("Dry");
   
       }
 
@@ -198,41 +225,59 @@ void loop() { // enclose all in if so its only if switch is high
  
  int frequencies[] = { 523, 587, 659, 698, 784, 880, 987, 1046 };
 
-  // Play each note in the tune
-if (!running && sliderValue > 700) {
-  for (int i = 0; i < 8; i++) {
-    tone(BUZZER_PIN, frequencies[i], 15);
-    delay(100); 
-  } // alarm
-  delay(1000);
-}
+
+if (millis() >= alarmStartTime) {
+    if (!alarmTriggered) {
+      running = false;
+      // Play the alarm sound
+      int frequencies[] = {523, 587, 659, 698, 784, 880, 987, 1046};
+      for (int i = 0; i < 8; i++) {
+        tone(BUZZER_PIN, frequencies[i], 40); 
+        delay(40); 
+      }
+      delay(1000); // Delay after the sequence
+    }
+    if (sliderValue <= 700) {
+        alarmTriggered = true; // stop it
+    }
+  }
+
 
 
   delay(10);
+  runCount++;
 }
-
-
-void setAlarm() {
-  
-  delay(ALARM_HOURS * 60 * 60 * 1000); // to ms, 8 hours rec 
-
-    running = false; // this triggers alarm in loop()
-
-
-}
-
 
 void printWithFormatting(const char* text) {
-    int length = 0;
-
+  int length = 0;
   if (cursorX == 0) {
-    lcd.print("Warnings: ");
+    lcd.clear();
+    //time stamping
+    DateTime now = rtc.now();
+    int minute = now.minute();
+    int hour = now.hour(); 
+    lcd.print(hour, DEC);
+    lcd.print(':');
+    lcd.print(minute < 10 ? "0" : ""); // Leading zero for minutes
+    lcd.print(minute, DEC);
+    lcd.print(' ');
     lcd.setCursor(0,1);
     lcd.print(text);
     while (text[length] != '\0') {
         length++;
     }
   } else {
+    lcd.setCursor(cursorX, 0);
+    DateTime now = rtc.now();
+    int minute = now.minute();
+    int hour = now.hour(); 
+    lcd.print(hour, DEC);
+    lcd.print(':');
+    lcd.print(minute < 10 ? "0" : ""); // Leading zero for minutes
+    lcd.print(minute, DEC);
+    lcd.print(' ');
+    lcd.setCursor(cursorX,1);
+    lcd.print(text);
     lcd.print(", ");
     lcd.print(text);
     while (text[length] != '\0') {
@@ -240,6 +285,9 @@ void printWithFormatting(const char* text) {
     }
   } 
 
-
-  cursorX = length;
+if (cursorX >= 8) {
+   lcd.autoscroll();
 }
+
+  cursorX = length + 2; 
+} 
