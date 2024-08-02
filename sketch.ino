6747140f8331b3e633975961cd11cc20de9f24a4
Cam <cameronmanzo111@gmail.com>
2024-08-02 00:20:02 -0400

    temp stuff, switch

--- sketch.ino
+++ sketch.ino
@@ -2,17 +2,34 @@
 #define LDR_PIN 2
 #define MOTION_PIN 16
 #define TEMP_PIN 26 // temperature 
+#define SLIDER_PIN 27
 
-int motionState = LOW;            
-int detectedMotion = 0;     
-const int motionCountTillWarning = 3;
+//motion
+int detectedMotion = 0;
+const int motionCountTillWarning = 5; // just in case user gets up before clock off , extra movements
 bool motionWarning = false;
+int motionState = LOW;
 
-
+//light
 int lightState = HIGH;
 int isLit = 0;
 bool lightWarning = false;
-const int lightCountTilWarning = 5;
+const int lightCountTilWarning = 7;
+
+bool tempHighWarning = false;
+bool tempLowWarning = false;
+int tempTooHighCount = 0;
+int tempTooLowCount = 0;
+const int tempHighCountTilWarning = 6;
+const int tempLowCountTilWarning = 6;
+const float TEMP_THRESHOLD_HIGH = 23.9; // (celsius) range can be changed but this is good for safety from research  
+const float TEMP_THRESHOLD_LOW = 12.7;
+const float BETA = 3950; // for this specific thermistor
+
+
+
+bool running = true; // disable to stop the loop
+
 
 
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
@@ -28,61 +45,95 @@ uint8_t heart[8] = {
   0b00000,
 };
 
-void setup() { 
+void setup() {
   pinMode(MOTION_PIN, INPUT);
   pinMode(LDR_PIN, INPUT);
-  Serial1.begin(11520);  // Start serial communication
-  lcd.begin(16, 2);
-  
-  lcd.createChar(0, heart);
-  
-
+  pinMode(SLIDER_PIN, INPUT);
+  pinMode(TEMP_PIN, INPUT);
+    lcd.begin(16, 2);
+      lcd.createChar(0, heart);
+  lcd.clear();  
+  lcd.setCursor(0, 0); 
   lcd.print("On");
 
+  // Serial1.begin(11520);  // Start serial communication bugging lcd maybe
 
-  Serial1.println("Setup complete");
 
-if (isLit != 0) {
+
   isLit = 0;
-}
-if (detectedMotion != 0) {
   detectedMotion = 0;
+  running = true;
 }
+
+void loop() { // enclose all in if so its only if switch is high
+
+  int sliderValue = analogRead(SLIDER_PIN);
+
+  motionState = digitalRead(MOTION_PIN);
+   lightState = digitalRead(LDR_PIN);
+
+  if (sliderValue > 700 && running = true;) {
+       lcd.clear();  
+    //motion
+    if (motionWarning == false) {
+      if (motionState == HIGH) {
+        
+        detectedMotion++;
+        delay(3000); // in case of extended toss and turn
+      }
+      if (detectedMotion >= motionCountTillWarning) {
+        motionWarning = true;
+        lcd.println("OVER MOTION LIMIT");
+      }
+    }
+
+
+    //light
+    if (lightWarning == false) {
+      if (lightState == LOW) {
+        isLit++;
+        delay(1000);
+      }
+      if (isLit >= lightCountTilWarning) {
+        lightWarning = true;
+
+      }
+    }
+
+    if (tempHighWarning == false) {
+      int thermistorReading = analogRead(TEMP_PIN);
+      float celsius = 1 / (log(1 / (1023. / thermistorReading - 1)) / BETA + 1.0 / 298.15) - 273.15;
+
+      if (celsius > TEMP_THRESHOLD_HIGH) {
+          tempTooHighCount++;
+           lcd.print("HIGH TEMP WARNING");
+          delay(1000);
+      }
+      if (tempTooHighCount >= tempHighCountTilWarning) {
+          tempHighWarning = true;
+      }
+    }
+        if (tempLowWarning == false) {
+      int thermistorReading = analogRead(TEMP_PIN);
+      float celsius = 1 / (log(1 / (1023. / thermistorReading - 1)) / BETA + 1.0 / 298.15) - 273.15;
+
+      if (celsius < TEMP_THRESHOLD_LOW) {
+          tempTooLowCount++;
+          lcd.print("LOW TEMP WARNING");
+          delay(1000);
+      }
+      if (tempTooLowCount >= tempLowCountTilWarning) {
+          tempLowWarning = true;
   
-}
+      }
 
-void loop() { // enclose all in if so its only if button is pressed
-  // motion
-motionState = digitalRead(MOTION_PIN);
-  lcd.setCursor(2, 0);
-if (motionWarning == false) {
-  if (motionState == HIGH) {
-    lcd.print("MOTION DETECTED");
-    detectedMotion++;
+    }
+
+
   }
-  if (detectedMotion >= motionCountTillWarning) {
-    motionWarning = true;
-    lcd.print("OVER MOTION LIMIT");
-  }
+
+  delay(10);
 }
 
 
-  //light
-lightState = digitalRead(LDR_PIN);
-if (lightWarning == false) {
-  if (lightState == LOW) {
-    isLit++;
-    lcd.print(isLit);
-  }
-  if (isLit >= lightCountTilWarning) {
-      lightWarning = true;
-      
-  }
-}
-
-  
-
-
-  delay(200);
-
-}
+// after 8 hours, disabale
